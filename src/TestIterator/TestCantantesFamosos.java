8fd16eefda80cb84735d330662f9dbae15e1f569
Raul Burgos Murray <raulburgos007@gmail.com>
2015-03-17 12:04:30 -0400

    Segundo comit de optimizacion

--- src/TestIterator/TestCantantesFamosos.java
+++ src/TestIterator/TestCantantesFamosos.java
@@ -8,18 +8,15 @@ public class TestCantantesFamosos {
     public static void main(String[] args){
         ListaCantantesFamosos listaCantantes;
         listaCantantes=new ListaCantantesFamosos();
-        CantanteFamoso cantante1 = new CantanteFamoso("uno","disco1");        
-        listaCantantes.addCantanteFamoso(cantante1);
-        CantanteFamoso cantante2 = new CantanteFamoso("dos","disco2");
-        listaCantantes.addCantanteFamoso(cantante2);
+        listaCantantes.addCantanteFamoso(new CantanteFamoso("Cantante1", "Disco1"));
+        listaCantantes.addCantanteFamoso(new CantanteFamoso("Cantante2", "Disco2"));
         listaCantantes.listarCantantes();
         EntradaDeTexto nombre,disco;
         System.out.println("Ingrese un nuevo cantane: ");
         nombre = new EntradaDeTexto();
         System.out.println("Ingrese un nombre de disco para el cantante: ");
         disco = new EntradaDeTexto();
-        CantanteFamoso cantante3 = new CantanteFamoso(nombre.getTextoCapitalizado(),disco.getTextoCapitalizado());
-        listaCantantes.addCantanteFamoso(cantante3);
+        listaCantantes.addCantanteFamoso(new CantanteFamoso(nombre.getTextoCapitalizado(),disco.getTextoCapitalizado()));
         listaCantantes.listarCantantes();       
         System.out.println("¿Desea continuar registrando cantantes y discos? (s/n):");
         EntradaDeTexto respuesta = new EntradaDeTexto();
