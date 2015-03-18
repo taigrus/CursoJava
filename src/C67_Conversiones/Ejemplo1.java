5f0f498f5382471c7560d41e9d59aa24ac254cc5
Raul Burgos Murray <raulburgos007@gmail.com>
2015-03-18 10:27:21 -0400

    Incrementada clase para prueba de conversiones

---
+++ src/C67_Conversiones/Ejemplo1.java
@@ -0,0 +1,13 @@
+package C67_Conversiones;
+
+import javax.swing.JOptionPane;
+
+public class Ejemplo1 {
+    public static void main (String[] Args) {
+        Integer entradaInt=0;
+    	String entradaUsuario = JOptionPane.showInputDialog ( "Introduzca un número:" );
+    	//entradaUsuario=(Integer)entradaUsuario;
+        entradaInt = entradaInt.valueOf(entradaUsuario);
+        System.out.println ("El doble del número introducido es: " + 2*entradaInt);    	
+    }
+}
