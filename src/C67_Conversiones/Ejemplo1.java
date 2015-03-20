40a2c3020e252de98df68cb6822006cb6dd86c4a
Raul Burgos Murray <raulburgos007@gmail.com>
2015-03-20 10:06:01 -0400

    arreglada codificacion en windows

--- src/C67_Conversiones/Ejemplo1.java
+++ src/C67_Conversiones/Ejemplo1.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 public class Ejemplo1 {
     public static void main (String[] Args) {
         Integer entradaInt=0;
-    	String entradaUsuario = JOptionPane.showInputDialog ( "Introduzca un número:" );
+    	String entradaUsuario = JOptionPane.showInputDialog ( "Introduzca un número entero:" );
         entradaInt=entradaInt.valueOf(entradaUsuario);
         //String salida = JOptionPane.showInternalMessageDialog(null,"El doble del número introducido es: ","Mnesaje",JOptionPane.INFORMATION_MESSAGE);
         System.out.println ("El doble del número introducido es: " + 2*entradaInt + " y el cuadrado del número que introdujo, es: " + entradaInt*entradaInt);    	
